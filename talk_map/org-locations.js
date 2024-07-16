40d81839441f3918cff60620ea244c85aca539df
majid-daliri <112280828+majid-daliri@users.noreply.github.com>
2024-07-16 12:19:34 -0400

    Update locations

--- talk_map/org-locations.js
+++ talk_map/org-locations.js
@@ -1,6 +1,6 @@
 var addressPoints = [
   [
-    "Honululu, Hawaii, US | ICML 2023 | <a href='https://github.com/majid-daliri/majid-daliri.github.io/raw/main/resources/ICML_Poster.png'>Accelerating Transformers via Kernel Density Estimation</a>",
+    "Honolulu, Hawaii, US | ICML 2023 | <a href='https://github.com/majid-daliri/majid-daliri.github.io/raw/main/resources/ICML_Poster.png'>Accelerating Transformers via Kernel Density Estimation</a>",
     21.315603,
     -157.858093
   ],
@@ -13,6 +13,10 @@ var addressPoints = [
     "San Diego, CA, USA | PLDI 2022 | <a href='https://github.com/majid-daliri/majid-daliri.github.io/raw/main/resources/PLDI_Slides.pdf'>Efficient Approximations for Cache-conscious Data Placement</a>",
     32.715736,
     -117.161087
-  ], 
-  // ... Add more entries here
+  ],
+  [
+    "Alexandria, VA, USA | SOSA 2024 | <a href='https://raw.githubusercontent.com/majid-daliri/majid-daliri.github.io/main/resources/SOSA_Presentation.pdf'>Simple Analysis of Priority Sampling</a>",
+    38.804836,
+    -77.046921
+  ] 
 ];
